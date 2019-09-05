a6d2aee65ec8166896f04abc3b08ffd338e24aad
Stephen Thompson <s.thompson@ucl.ac.uk>
2019-09-05 10:28:49 +0100

    Issue #6 removed unused code from cone and sphere

--- sksurgerytrackervisualisation/shapes/cone.py
+++ sksurgerytrackervisualisation/shapes/cone.py
@@ -38,19 +38,14 @@ class VTKConeModel(vbs.VTKSurfaceModel):
 
         #this is from super init, have to redo as we now have data
         self.normals = None
-        if self.source.GetPointData().GetNormals() is None:
-            self.normals = vtk.vtkPolyDataNormals()
-            self.normals.SetInputData(self.source)
-            self.normals.SetAutoOrientNormals(True)
-            self.normals.SetFlipNormals(False)
+        self.normals = vtk.vtkPolyDataNormals()
+        self.normals.SetInputData(self.source)
+        self.normals.SetAutoOrientNormals(True)
+        self.normals.SetFlipNormals(False)
         self.transform = vtk.vtkTransform()
         self.transform.Identity()
         self.transform_filter = vtk.vtkTransformPolyDataFilter()
-        if self.normals is None:
-            self.transform_filter.SetInputData(self.source)
-        else:
-            self.transform_filter.SetInputConnection(
-                self.normals.GetOutputPort())
+        self.transform_filter.SetInputConnection(self.normals.GetOutputPort())
         self.transform_filter.SetTransform(self.transform)
         self.mapper = vtk.vtkPolyDataMapper()
         self.mapper.SetInputConnection(self.transform_filter.GetOutputPort())
